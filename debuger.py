22187267d34fe649ff38d97e31f788673093316b
SrZuria <145557685+SrZuria@users.noreply.github.com>
2023-09-29 19:07:45 -0300

    Add files via upload

---
+++ debuger.py
@@ -0,0 +1,34 @@
+# if / elif      / else
+# se / se não se / se não
+
+"""
+debugger é usado para voce ver como o interpretador esta 
+funcionando, ele ira checar linha a linha.
+Nesse caso como existe condições, ele entra no primeiro if
+e se ele for verdadeiro ele ja pula qualquer outra condição,
+ele nao ira nem mesmo checar as outras pois a primeira ja 
+foi satisfeita
+"""
+
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
