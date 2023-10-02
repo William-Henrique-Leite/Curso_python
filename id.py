79af00b90cda70bd6b0e39395919a49412a46fa5
SrZuria <145557685+SrZuria@users.noreply.github.com>
2023-10-02 10:52:23 -0300

    Create id.py

---
+++ id.py
@@ -0,0 +1,14 @@
+"""
+flag = serve para marcar um lugar
+none = nao valor
+is e is not = é ou nao é (tipo, valor, identidade)
+id = identidade
+desa forma voce pode ver o local da memoria desse dado
+"""
+
+v1 = "a"
+v2 = "a"
+v3 = "b"
+print(id(v1))
+print(id(v2))
+print(id(v3))
